5fddb79648fc6a7b477e89661ff0da7f0fd57be3
Alejandro De León Languré <106360071+langure@users.noreply.github.com>
2022-07-20 17:05:24 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import os
+
+# ENV
+EXCEL_FILE = "metadatos.xlsx"
+JSON_FILE = "metadatos.json"
+
+def clear_screen():
+    if os.name == 'posix':
+        _ = os.system('clear')
+    else:    
+        _ = os.system('cls')
+
+if __name__ == "__main__":
+    clear_screen()
+    print("Hello world")
