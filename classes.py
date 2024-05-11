dde20f8f59d463f2201ab319e234668fc1fcaeae
Alejandro De León Languré <106360071+langure@users.noreply.github.com>
2024-05-11 09:41:18 +0900

    Restoration to idea

--- classes.py
+++ classes.py
@@ -10,6 +10,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
 
 # Constants
 
@@ -288,19 +289,21 @@ def read_from_google():
     creds = None
     panda = None
     
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+    #if os.path.exists('token.json'):
+        #creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        
+    #if not creds or not creds.valid:
+    #    if creds and creds.expired and creds.refresh_token:
+    #        creds.refresh(Request())
+    #    else:
+    #       flow = InstalledAppFlow.from_client_secrets_file(
+    #           'credentials.json', SCOPES)
+    #       creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    #    with open('token.json', 'w') as token:
+    #       token.write(creds.to_json())
     try:
+        creds = service_account.Credentials.from_service_account_file('triple-rookery-360218-1f50cc59d0e9.json', scopes=SCOPES)
         service = build('sheets', 'v4', credentials=creds)
 
         # Call the Sheets API
